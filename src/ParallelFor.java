e3aa20d12b00739ccf6417a6c2e53c3de69a732a
Abhinav Sarje <abhinav@onera.com>
2017-10-16 11:58:41 -0700

    initial commit

---
+++ src/ParallelFor.java
@@ -0,0 +1,27 @@
+import java.util.Random;
+
+public class ParallelFor {
+
+  private static final int MIN_RAND = 0;
+  private static final int MAX_RAND = 10000;
+  private static final int SIZE = 6400000;
+
+  public static void main(String[] args) {
+    Random rand = new Random();
+    Integer[] numbers = new Integer[SIZE];
+
+    for(int i = 0; i < numbers.length; ++i) {
+      numbers[i] = rand.nextInt(MAX_RAND - MIN_RAND + 1) + MIN_RAND;
+    }
+
+    Integer sum = 0;
+
+    // omp parallel for public(sum)
+    for(int i = 0; i < numbers.length; ++i) {
+      sum += numbers[i];
+    }
+
+    System.out.println("RESULT: " + sum);
+  }
+
+}
